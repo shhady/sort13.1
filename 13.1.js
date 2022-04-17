663db0d12463215d5df5586078100a299b75439e
shhady <shhadyse@gmail.com>
2022-04-17 21:41:40 +0300

    sort13.1

---
+++ 13.1.js
@@ -0,0 +1,7 @@
+const numbers = [1, -5, 666, 2, 400, 11];
+
+let asc = numbers.slice().sort((a, b) => a - b);
+console.log(asc);
+
+// let des = numbers.slice().sort((a, b) => b - a);
+// console.log(des);
